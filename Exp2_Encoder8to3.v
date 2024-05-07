8effc62747227d9a988fcdcd4df8a6be871bbcb9
Rishany2004 <159290227+Rishany2004@users.noreply.github.com>
2024-05-07 14:39:58 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,7 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
-
-
-
+module enco83(I,G0,G1,G2);
+input [7:0]I;
+output G0,G1,G2;
+or g1(G0,I[1],I[3],I[5],I[7]);
+or g2(G1,I[2],I[3],I[6],I[7]);
+or g3(G2,I[7],I[6],I[5],I[4]);
 endmodule
